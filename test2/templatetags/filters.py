e67de7fbaca6dc1be09beb05c59a89c5fcd816c0
masha <masha@masha-netbuk.(none)>
2014-08-25 21:31:36 +0400

    Дофига всего в дизайне, работающие иконки.

--- test2/templatetags/filters.py
+++ test2/templatetags/filters.py
@@ -2,9 +2,11 @@
 # Фильтры
 
 from django import template
+from django.utils.safestring import mark_safe
+
 
 register = template.Library()
 
 @register.filter(name='price')
 def price(number):
-	return (unicode(number) + u" \u20BD")
+	return (mark_safe(str(number) + ' <i class="fa fa-rub"></i>') )
